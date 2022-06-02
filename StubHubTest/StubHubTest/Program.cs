1cf8321e407000c3f186cc9a20f6c19a006fa717
Zeeshan Akhtar <zeeshan.dhool@live.com>
2022-06-02 23:51:15 +0400

    Updated code after optimization

--- StubHubTest/StubHubTest/Program.cs
+++ StubHubTest/StubHubTest/Program.cs
@@ -18,17 +18,21 @@ namespace Viagogo
     }
     public class Solution
     {
+      public static  Dictionary<string, double> dictionaryOfSameCity = new Dictionary<string, double>();
         static void Main(string[] args)
         {
             var events = new List<Event>{
-                            new Event{ Name = "LadyGaga", City = "Chicago", Price=200},
-                            new Event{ Name = "LadyGaga", City = "New York", Price=250},
-                            new Event{ Name = "U2", City = "New York", Price=150},
-                            new Event{ Name = "Dua Lipa", City = "New York", Price=175},
-                            new Event{ Name = "U2", City = "Washington", Price = 300},
-                            new Event{ Name = "Rolling Stones", City = "Los Angeles", Price = 200},
-                            new Event{ Name = "Rolling Stones", City = "Pittsburgh", Price = 175}
-                            };
+                        new Event{ Name = "Phantom of the Opera", City = "New York"},
+                        new Event{ Name = "Metallica", City = "Los Angeles"},
+                        new Event{ Name = "Metallica", City = "New York"},
+                        new Event{ Name = "Metallica", City = "Boston"},
+                        new Event{ Name = "LadyGaGa", City = "New York"},
+                        new Event{ Name = "LadyGaGa", City = "Boston"},
+                        new Event{ Name = "LadyGaGa", City = "Chicago"},
+                        new Event{ Name = "LadyGaGa", City = "San Francisco"},
+                        new Event{ Name = "LadyGaGa", City = "Washington"}
+                        };
+
 
             var customer = new List<Customer> { new Customer { Name = "John Smith", City = "New York" } };
             var query = customer.Where(x => x.Name == "John Smith").ToList();
@@ -36,60 +40,72 @@ namespace Viagogo
             Console.WriteLine("Send Email to customer with Same city sorted by Price \n");
             foreach (var cus in query)
             {
-                foreach (var ev in events.OrderBy(x => x.Price).Where(x => x.City == cus.City))
+                foreach (var ev in events.Where(x => x.City == cus.City))
                 {
                     AddToEmail(cus, ev);
                 }
+
+
             }
 
             Console.WriteLine("\n Send Email to customer with nearest 5 cities sorted by Price \n");
             foreach (var cus in query)
             {
-                var notSameCityEvents = events.Where(x => x.City != cus.City);
-                foreach (var ev in notSameCityEvents)
-                {
-                    ev.Distance = GetDistance(cus.City, ev.City);
-                }
-                foreach (var ev in notSameCityEvents.Where(x => x.Distance != -1).OrderBy(x => x.Price).Take(5))
+                var custEvents = events.Select(x => new Event { Distance = GetDistance(cus.City, x.City), Name = x.Name, City = x.City }).ToList();
+                foreach (var ev in custEvents.Where(x => x.Distance != -1).OrderBy(x => x.Distance).Take(5))
                 {
                     AddToEmail(cus, ev);
                 }
             }
+
+            Console.WriteLine("\n Optimized for 3rd question \n");
+            foreach (var cus in query)
+            {
+                var custEvents = events.Select(x => new Event { Distance = CheckAndGetDistance(cus.City, x.City), Name = x.Name, City = x.City }).ToList();
+                foreach (var ev in custEvents.Where(x => x.Distance != -1).OrderBy(x => x.Distance).Take(5))
+                {
+                    AddToEmail(cus, ev);
+                }
+            }
+        }
+
+        static double CheckAndGetDistance(string fromCity, string toCity)
+        {
+            double distance = 0;
+            if (dictionaryOfSameCity.ContainsKey(toCity))
+            {
+                distance = dictionaryOfSameCity[toCity];
+            }
+            else
+            {
+                distance = GetDistance(fromCity, toCity);
+                dictionaryOfSameCity.Add(toCity, distance);
+            }
+            return distance;
         }
         static void AddToEmail(Customer c, Event e)
         {
             Console.Out.WriteLine($"Customer: {c.Name}, Event: {e.Name}" + (e.Distance > 0 ? $", ({e.Distance} miles away)" : "") + ($" for ${e.Price}"));
         }
 
-        static double GetDistance(string fromCity, string toCity)
+        static int GetDistance(string fromCity, string toCity)
         {
-            double distance = -1;
-            try
+            return AlphebiticalDistance(fromCity, toCity);
+        }
+
+        private static int AlphebiticalDistance(string s, string t)
+        {
+            var result = 0;
+            var i = 0;
+            for (i = 0; i < Math.Min(s.Length, t.Length); i++)
             {
-                switch (toCity)
-                {
-                    case "Chicago":
-                        distance = 711.11;
-                        break;
-                    case "Washington":
-                        distance = 203.46;
-                        break;
-                    case "Los Angeles":
-                        distance = 2445.47;
-                        break;
-                    case "Pittsburgh":
-                        distance = 314.72;
-                        break;
-                    default:
-                        distance = 0;
-                        break;
-                }
+                result += Math.Abs(s[i] - t[i]);
             }
-            catch (Exception ex)
+            for (; i < Math.Max(s.Length, t.Length); i++)
             {
-                return distance;
+                result += s.Length > t.Length ? s[i] : t[i];
             }
-            return distance;
+            return result;
         }
     }
 }
